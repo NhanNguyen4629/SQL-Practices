805f5e55cb9409fab6ed191b471ef4c8a54bbd92
NhanNguyen4629 <trinhnguyenthanhnhan26@gmail.com>
2024-06-04 23:52:02 +0700

    Update project3.sql

--- project3.sql
+++ project3.sql
@@ -1,28 +1,38 @@
---1
+--1.Doanh thu theo từng ProductLine, Year  và DealSize
 select productline,year_id,dealsize,
 sum(quantityordered*priceeach) as revenue
 from public.sales_dataset_rfm_prj
 group by productline,year_id,dealsize
 order by year_id
---2
-select month_id, sum(quantityordered*priceeach) as revenue, 
+  
+--2. Đâu là tháng có bán tốt nhất mỗi năm?
+select year_id,month_id, sum(quantityordered*priceeach) as revenue, 
 count(ordernumber) as order_number from public.sales_dataset_rfm_prj
-group by month_id
-order by sum(quantityordered*priceeach) desc
---3
+group by year_id,month_id
+order by year_id,sum(quantityordered*priceeach) desc
+  --Trả lời: năm 2003 và 2004: tháng 11
+  
+--3.Product line nào được bán nhiều ở tháng 11?
 select month_id,productline,
 sum(quantityordered*priceeach) as revenue,
+count(ordernumber) as order_number,
 dense_rank() over (order by sum(quantityordered*priceeach) desc) as rank
 from public.sales_dataset_rfm_prj
 where month_id=11
 group by month_id,productline
---4
-select year_id, productline, sum(quantityordered*priceeach) as revenue,
+  --Trả lời: Classic Cars
+  
+--4.Đâu là sản phẩm có doanh thu tốt nhất ở UK mỗi năm? 
+select*from(select year_id, productline, sum(quantityordered*priceeach) as revenue,
 dense_rank() over (partition by year_id order by sum(quantityordered*priceeach) desc) as rank
 from public.sales_dataset_rfm_prj
 where country='UK'
-group by year_id, productline
---5
+group by year_id, productline) as top1
+where rank=1
+  --2003 và 2004: Classic Cars
+  --2005:Motorcycles 
+  
+--5.Ai là khách hàng tốt nhất, phân tích dựa vào RFM 
 with customer_rfm as(select customername,
 current_date-max(orderdate) as R,
 Count(ordernumber) as F,
@@ -42,3 +52,4 @@ from rfm_score as a
 join public.segment_score as b on a.rfm_score = b.scores
 group by b.segment
 order by count(a.customername) desc
+--Trả lời: khách hàng tiềm năng nhất là New Customers  
